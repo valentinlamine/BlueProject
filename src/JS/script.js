0ea4b71215851ee0081b019197168829133ec9da
valentin lamine <gitlab@valentinlamine.fr>
2023-09-11 16:21:02 +0200

    transporting the design from TP-WEB

---
+++ src/JS/script.js
@@ -0,0 +1,11 @@
+var leftImage = document.getElementById('left-image');
+var rightImage = document.getElementById('right-image');
+
+var leftChoiceLink = document.getElementById('left-choice-link');
+var rightChoiceLink = document.getElementById('right-choice-link');
+
+var leftText = document.getElementById('left-text');
+var rightText = document.getElementById('right-text');
+
+var mainTitle = document.getElementById('main-title');
+var mainText = document.getElementById('main-text');
