3fec09691724bc1eab6ba6e5cc2adc4e04520752
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2023-09-12 16:18:17 +0200

    don't worry be happy

--- src/main.go
+++ src/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"src/backend"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	backend.StartServer()
+	r := mux.NewRouter()
+	r.PathPrefix("/ASSETS/").Handler(http.StripPrefix("/ASSETS/", http.FileServer(http.Dir("/ASSETS"))))
+	r.PathPrefix("/CSS/").Handler(http.StripPrefix("/CSS/", http.FileServer(http.Dir("/CSS"))))
+	r.PathPrefix("/JS/").Handler(http.StripPrefix("/JS/", http.FileServer(http.Dir("/JS"))))
+
+	//handle routing
+	r.HandleFunc("/", backend.IndexHandler)
+
+	fmt.Println("server is running on port 8080 : http://localhost:8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
+
 }
