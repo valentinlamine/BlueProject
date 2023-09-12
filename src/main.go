8614b27f431c319aba6ce2125693f923b026b5d1
valentin lamine <gitlab@valentinlamine.fr>
2023-09-12 17:06:38 +0200

    correcting merge v2

--- src/main.go
+++ src/main.go
@@ -11,17 +11,10 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-<<<<<<< HEAD
+
 	r.PathPrefix("/ASSETS/").Handler(http.StripPrefix("/ASSETS/", http.FileServer(http.Dir("./frontend/ASSETS"))))
 	r.PathPrefix("/CSS/").Handler(http.StripPrefix("/CSS/", http.FileServer(http.Dir("./frontend/CSS"))))
 	r.PathPrefix("/JS/").Handler(http.StripPrefix("/JS/", http.FileServer(http.Dir("./frontend/JS")))) //handle routing
-=======
-	r.PathPrefix("./ASSETS/").Handler(http.StripPrefix("./ASSETS/", http.FileServer(http.Dir("./ASSETS/"))))
-	r.PathPrefix("./CSS/").Handler(http.StripPrefix("./CSS/", http.FileServer(http.Dir("./CSS/"))))
-	r.PathPrefix("./JS/").Handler(http.StripPrefix("./JS/", http.FileServer(http.Dir("./JS/"))))
-
-	//handle routing
->>>>>>> 49e689cb3fb272c5b58a9701e1086a77d0c8c127
 	r.HandleFunc("/", backend.IndexHandler)
 
 	fmt.Println("server is running on port 8080 : http://localhost:8080")
